6d2a40f5dc69c110fefe56799c81f22353db6f4a
CharlieCondorcet <charliecondorcet@gmail.com>
2020-05-29 15:00:44 -0400

    feat(ContratosImpl.cs): add file to implement the contracts

---
+++ ContratosImpl.cs
@@ -0,0 +1,10 @@
+using Fivet.Dao;
+using Fivet.ZeroIce.model;
+using Ice;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging; 
+
+namespace Fivet.ZeroIce
+{
+    
+}
