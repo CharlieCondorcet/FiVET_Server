ee882bcc08d46462536e24f455f5390c71a430a8
CharlieCondorcet <charliecondorcet@gmail.com>
2020-05-29 17:06:55 -0400

    feat(TheSystemImpl): add the implementation to TheSystem

---
+++ TheSystemImpl.cs
@@ -0,0 +1,17 @@
+using System; 
+using Fivet.ZeroIce.model;
+using Ice;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Fivet.ZeroIce
+{
+    /// <summary>
+    /// The Implementation  of TheSystem interface.
+    /// </summary>
+    public class TheSystemImpl : TheSystemDisp_
+    {
+        
+    }
+
+}
